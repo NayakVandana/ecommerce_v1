32b91289c4a04813984733525bc834d0356d6d73
Vandana Nayak <vandana.nayak@binarysphere.com>
2026-02-21 18:30:20 +0530

    Recently Viewed link

--- resources/js/Components/Navigation.tsx
+++ resources/js/Components/Navigation.tsx
@@ -278,6 +278,14 @@ export default function Navigation() {
                                                     Orders
                                                 </Link>
                                                 <Link
+                                                    href="/recently-viewed"
+                                                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 flex items-center"
+                                                    onClick={() => setUserMenuOpen(false)}
+                                                >
+                                                    <TagIcon className="h-5 w-5 mr-2" />
+                                                    Recently Viewed
+                                                </Link>
+                                                <Link
                                                     href="/profile"
                                                     className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 flex items-center"
                                                     onClick={() => setUserMenuOpen(false)}
